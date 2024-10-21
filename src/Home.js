4f97182479e587014f57de0b0fc00131f20ae610
DainShin <di.shin0611@gmail.com>
2024-10-21 21:02:47 +0900

    Added BlogList

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,20 @@
+import { useState } from "react";
+import BlogList from "./BlogList";
+
 const Home = () => {
-    return (  
+
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ]);
+
+    return (
         <div className="home">
-            <h2>Hompepage</h2>
+          <BlogList blogs={blogs} title="All Blogs" />
+          <BlogList blogs={blogs.filter((blog) => blog.author == 'mario')} title="Mario's Blogs" />
         </div>
     );
 }
- 
+
 export default Home;
-<div className="home">
-    <h2>Hompepage</h2>
-</div>

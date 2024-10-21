e848f261f15269d1d4a1c02e626addb86b1b7d62
DainShin <di.shin0611@gmail.com>
2024-10-21 22:08:40 +0900

    tested useEffect funtion

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -9,10 +9,27 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ]);
 
+    const [name,setName] = useState('mario');
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    }
+
+    // useEffect function is called whenever the page is rendered
+    // If we want to make this function work only when they are rendered, add the empty array as a second parameter
+    // If we want to call this function only when the name value is changed from the initial value to the new value, add an array with the "name" value
+    useEffect(() => {
+        console.log('use effect ran');
+        console.log(name);
+    }, [name]);
+
     return (
         <div className="home">
-          <BlogList blogs={blogs} title="All Blogs" />
-          <BlogList blogs={blogs.filter((blog) => blog.author == 'mario')} title="Mario's Blogs" />
+          <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
+          <button onClick={() => setName('luigi')}>change name</button>
+          <p>{ name }</p>
+          {/* <BlogList blogs={blogs.filter((blog) => blog.author == 'mario')} title="Mario's Blogs" /> */}
         </div>
     );
 }
